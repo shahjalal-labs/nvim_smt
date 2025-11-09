a219b464271b6a3b12e5552a64cf5b11a8eb4bae
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-09 20:35:30 +0600

    refactor: Updated custom/init.lua | chore: Removed blockman/blockman.lua, blockman/init.lua

--- lua/sj/core/custom/blockman/init.lua
+++
@@ -1,8 +0,0 @@
-local blockman = require("sj.core.custom.blockman.blockman")
-
-blockman.setup({
-	enabled = true,
-})
-
--- Keymaps
-vim.keymap.set("n", "<leader>vb", "<cmd>BlockmanToggle<cr>", { desc = "Toggle Blockman" })
