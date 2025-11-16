14178f0e20f72da728ff0fdeafe80e14e9c92144
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-16 16:53:22 +0600

    refactor: Updated Terminal/nvimTerminal.lua

--- lua/sj/core/custom/TmuxZshCli/Terminal/nvimTerminal.lua
+++ lua/sj/core/custom/TmuxZshCli/Terminal/nvimTerminal.lua
@@ -5,38 +5,6 @@ vim.api.nvim_set_keymap("t", "<C-j>", [[<C-\><C-n><C-w>j]], { noremap = true }) 
 vim.api.nvim_set_keymap("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true }) -- Navigate up
 vim.api.nvim_set_keymap("n", "<leader>tz", ":split | terminal<CR>", { noremap = true, silent = true })
 
---[[ -- floating terminal
-function _G.floating_term()
-	-- create a new scratch buffer
-	local buf = vim.api.nvim_create_buf(false, true)
-
-	-- window size
-	local width = math.floor(vim.o.columns * 0.8)
-	local height = math.floor(vim.o.lines * 0.8)
-	local row = math.floor((vim.o.lines - height) / 2)
-	local col = math.floor((vim.o.columns - width) / 2)
-
-	-- open floating window
-	local win = vim.api.nvim_open_win(buf, true, {
-		relative = "editor",
-		width = width,
-		height = height,
-		row = row,
-		col = col,
-		style = "minimal",
-		border = "rounded",
-	})
-
-	-- turn the buffer into a terminal
-	vim.fn.termopen(vim.o.shell)
-
-	-- map "q" to close the terminal window
-	vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>bd!<CR>", { noremap = true, silent = true })
-end
-
--- keybinding to open it
-vim.keymap.set("n", "<leader>Tf", floating_term, { desc = "Floating Terminal" }) ]]
-
 -- floating terminal
 function _G.floating_term()
 	local buf = vim.api.nvim_create_buf(false, true)
@@ -69,4 +37,4 @@ function _G.floating_term()
 	vim.cmd("startinsert")
 end
 
-vim.keymap.set("n", "<leader>Tf", floating_term, { desc = "Floating Terminal" })
+vim.keymap.set("n", "<leader>tf", floating_term, { desc = "Floating Terminal" })
