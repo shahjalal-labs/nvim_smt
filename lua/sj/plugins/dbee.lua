1cfc7ceb589f709c7cc96c6aede6d2ab3b31ac4e
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-12-09 15:31:33 +0600

    refactor: Updated plugins/dbee.lua

--- lua/sj/plugins/dbee.lua
+++ lua/sj/plugins/dbee.lua
@@ -1,28 +1,15 @@
 return {
 	"kndndrj/nvim-dbee",
 	dependencies = {
-		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
+		"nvim-telescope/telescope.nvim",
 	},
-	build = ":DBee install",
 	config = function()
-		-- Simple keymap that works
-		vim.keymap.set("n", "<leader>db", "<cmd>DBee<CR>", { desc = "Open DBee" })
-
-		-- Wait for binary to be ready
-		vim.defer_fn(function()
-			local binary = vim.fn.stdpath("data") .. "/dbee/bin/dbee"
-			if vim.fn.executable(binary) == 1 then
-				require("dbee").setup({
-					sources = {
-						require("dbee.sources").EnvVarsSource:new("DATABASE_URL"),
-					},
-				})
-				vim.notify("DBee ready!", vim.log.levels.INFO)
-			else
-				vim.notify("DBee binary missing. Run :DBee install", vim.log.levels.WARN)
-			end
-		end, 1000) -- Wait 1 second
+		require("dbee").setup({
+			prefix = "<leader>D",
+			connections = {
+				main = "postgres://postgres:sj@localhost:5432/postgre-basics", -- replace with your DB URL
+			},
+		})
 	end,
 }
