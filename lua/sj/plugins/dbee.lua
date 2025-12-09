cc2210e37003543b06e214269ec0b83ac44de7d0
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-12-09 15:17:33 +0600

    refactor: Updated lazy-lock.json, plugins/dbee.lua

--- lua/sj/plugins/dbee.lua
+++ lua/sj/plugins/dbee.lua
@@ -1,20 +1,28 @@
 return {
 	"kndndrj/nvim-dbee",
-	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	build = ":DBee install",
 	config = function()
-		local dbee = require("dbee")
+		-- Simple keymap that works
+		vim.keymap.set("n", "<leader>db", "<cmd>DBee<CR>", { desc = "Open DBee" })
 
-		dbee.setup({
-			prefix = "<leader>AD", -- your leader + D
-			default_connection = "main",
-			connections = {
-				main = os.getenv("DATABASE_URL"), -- make sure DATABASE_URL is set
-			},
-		})
-
-		-- optional: Telescope integration
-		pcall(function()
-			require("telescope").load_extension("dbee")
-		end)
+		-- Wait for binary to be ready
+		vim.defer_fn(function()
+			local binary = vim.fn.stdpath("data") .. "/dbee/bin/dbee"
+			if vim.fn.executable(binary) == 1 then
+				require("dbee").setup({
+					sources = {
+						require("dbee.sources").EnvVarsSource:new("DATABASE_URL"),
+					},
+				})
+				vim.notify("DBee ready!", vim.log.levels.INFO)
+			else
+				vim.notify("DBee binary missing. Run :DBee install", vim.log.levels.WARN)
+			end
+		end, 1000) -- Wait 1 second
 	end,
 }
