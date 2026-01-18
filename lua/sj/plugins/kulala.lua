79742939c97acb8772f88bcfa875b1f7a974f131
Backend Shahjalal <shahjalal.backend@gmail.com>
2026-01-18 09:20:23 +0600

    refactor: Updated plugins/kulala.lua

--- lua/sj/plugins/kulala.lua
+++ lua/sj/plugins/kulala.lua
@@ -1,19 +1,34 @@
-vim.filetype.add({
-	extension = {
-		["http"] = "http",
-	},
-})
-
 return {
 	"mistweaverco/kulala.nvim",
 	ft = { "http", "rest" },
-	opts = {
-		global_keymaps = false,
+	keys = {
+		{ "<leader>xr", desc = "Kulala: run request" },
+		{ "<leader>xa", desc = "Kulala: run all" },
+		{ "<leader>xe", desc = "Kulala: select env" },
+		{ "<leader>xo", desc = "Kulala: open scratchpad" },
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>Rs", "<cmd>KulalaSend<CR>", { desc = "Send request" })
-		vim.keymap.set("n", "<leader>Ra", "<cmd>KulalaSendAll<CR>", { desc = "Send all requests" })
-		vim.keymap.set("n", "<leader>Rb", "<cmd>KulalaScratchpad<CR>", { desc = "Kulala scratchpad" })
-		vim.keymap.set("n", "<leader>Re", "<cmd>KulalaSelectEnv<CR>", { desc = "Select Kulala env" })
+		require("kulala").setup({
+			global_keymaps = true,
+			global_keymaps_prefix = "<leader>R",
+			kulala_keymaps_prefix = "",
+		})
+
+		-- SAFE keymaps (after setup)
+		vim.keymap.set("n", "<leader>rr", function()
+			require("kulala").run()
+		end, { desc = "Kulala: run request" })
+
+		vim.keymap.set("n", "<leader>ra", function()
+			require("kulala").run_all()
+		end, { desc = "Kulala: run all" })
+
+		vim.keymap.set("n", "<leader>re", function()
+			require("kulala").select_env()
+		end, { desc = "Kulala: select env" })
+
+		vim.keymap.set("n", "<leader>ro", function()
+			require("kulala").open()
+		end, { desc = "Kulala: open scratchpad" })
 	end,
 }
